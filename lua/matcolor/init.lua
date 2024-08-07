00d932cd3a09a6b7ec4b4e9a3fc162ee46dd05bf
cube <icegame1118@gmail.com>
2024-08-08 02:05:08 +0900

    commit

--- lua/matcolor/init.lua
+++ lua/matcolor/init.lua
@@ -147,7 +147,7 @@ local function load(theme)
 	vim.cmd([[
         augroup DisableRainbowDelimiters
             autocmd!
-            autocmd ColorScheme matcolor lua require('rainbow-delimiters').enable()
+            autocmd ColorScheme matcolor lua require('rainbow-delimiters').disable()
         augroup END
     ]])
 
